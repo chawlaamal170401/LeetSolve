31f725308ea0d3796c06c5af6f99cf4d419197da
Amal Chawla <chawla.amal.170401@gmail.com>
2022-10-01 09:36:54 +0530

    Time: 3 ms (63.51%), Space: 6.2 MB (75.45%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,16 @@
+class Solution { 
+public:
+    int numDecodings(const string& s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        
+        dp[n] = 1;
+        
+        for (int i=n-1; i>=0; i--) {
+            if (s[i] != '0') dp[i] += dp[i+1];
+            if (i+1<n and (s[i] == '1' or s[i] == '2' and s[i+1] <= '6')) 
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
+    }
+};
