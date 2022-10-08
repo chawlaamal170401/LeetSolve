d34beaaabce8e9ebb55823ba90565473049acff6
Amal Chawla <chawla.amal.170401@gmail.com>
2022-10-08 12:27:53 +0530

    Time: 1046 ms (46.30%), Space: 135.9 MB (43.51%) - LeetHub

---
+++ 447-number-of-boomerangs/447-number-of-boomerangs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        int booms = 0;
+        for (auto &p : points) {
+            unordered_map<double, int> ctr(points.size());
+            for (auto &q : points)
+                booms += 2 * ctr[hypot(p[0] - q[0], p[1] - q[1])]++;
+        }
+        return booms;
+    }
+};
