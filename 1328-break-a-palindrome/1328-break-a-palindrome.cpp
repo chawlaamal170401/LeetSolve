e0a34080db4aff05f00385a9e5f0b527aac2121e
Amal Chawla <chawla.amal.170401@gmail.com>
2022-10-10 12:00:11 +0530

    Time: 3 ms (45.61%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string breakPalindrome(string p) {
+        int n = p.size();
+        if(n == 0 or p == "a") return "";
+        
+        for(int i=0; i<n/2; i++){
+            if(p[i] != 'a'){
+                p[i] = 'a';
+                return p;
+            }
+        }
+        
+        p[n-1] = 'b';
+        return n < 2 ? "" : p;
+    }
+};
