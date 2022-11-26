3397b70209d7596bdbb58687d0f4720fb829cf17
Amal Chawla <chawla.amal.170401@gmail.com>
2022-11-26 09:26:44 +0530

    Time: 837 ms (82.08%), Space: 156 MB (25.43%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        sort(events.begin(),events.end());
+        
+        int maxVal = 0, ans = 0;
+        priority_queue<vector<int>> pq;
+        
+        for(auto e : events){
+            while(!pq.empty() and ((-pq.top()[0]) < e[0])){
+                maxVal = max(maxVal , pq.top()[2]);
+                pq.pop();
+            }
+            ans = max(ans , e[2]+maxVal);
+            pq.push({(-e[1]) , e[0] , e[2]});
+        }
+        return ans;
+    }
+};
